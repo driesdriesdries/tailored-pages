5d01d70213572360545b35b03fd5790be9d5e798
Andries Bester <andriesbester@Andriess-MacBook-Air.local>
2024-05-31 13:36:10 +0200

    modified the listing land landing pages admin to show the assigned brand

--- tailored-pages.php
+++ tailored-pages.php
@@ -414,5 +414,83 @@ function tp_filter_products_by_brand($args, $field, $post_id) {
 
     return $args;
 }
+// Add custom columns to Listing Page post type
+function tp_add_listing_page_columns($columns) {
+    $new_columns = array();
+    foreach ($columns as $key => $value) {
+        if ($key == 'title') {
+            $new_columns[$key] = $value;
+            $new_columns['assigned_brand'] = __('Assigned Brand', 'text_domain');
+        } else {
+            $new_columns[$key] = $value;
+        }
+    }
+    return $new_columns;
+}
+add_filter('manage_listing-page_posts_columns', 'tp_add_listing_page_columns');
+
+// Populate custom columns for Listing Page post type
+function tp_custom_listing_page_column($column, $post_id) {
+    if ($column === 'assigned_brand') {
+        $associated_brand = get_post_meta($post_id, 'associated_brand', true);
+        if ($associated_brand) {
+            if (is_array($associated_brand)) {
+                $associated_brand = $associated_brand[0]; // If it is an array, get the first element
+            }
+            $brand_title = get_the_title($associated_brand);
+            $brand_link = get_edit_post_link($associated_brand);
+            echo '<a href="' . esc_url($brand_link) . '">' . esc_html($brand_title) . '</a>';
+        } else {
+            echo __('No Brand Assigned', 'text_domain');
+        }
+    }
+}
+add_action('manage_listing-page_posts_custom_column', 'tp_custom_listing_page_column', 10, 2);
+
+// Add custom columns to Landing Page post type
+function tp_add_landing_page_columns($columns) {
+    $new_columns = array();
+    foreach ($columns as $key => $value) {
+        if ($key == 'title') {
+            $new_columns[$key] = $value;
+            $new_columns['assigned_brand'] = __('Assigned Brand', 'text_domain');
+        } else {
+            $new_columns[$key] = $value;
+        }
+    }
+    return $new_columns;
+}
+add_filter('manage_landing-page_posts_columns', 'tp_add_landing_page_columns');
+
+// Populate custom columns for Landing Page post type
+function tp_custom_landing_page_column($column, $post_id) {
+    if ($column === 'assigned_brand') {
+        $associated_brand = get_post_meta($post_id, 'associated_brand', true);
+        if ($associated_brand) {
+            if (is_array($associated_brand)) {
+                $associated_brand = $associated_brand[0]; // If it is an array, get the first element
+            }
+            $brand_title = get_the_title($associated_brand);
+            $brand_link = get_edit_post_link($associated_brand);
+            echo '<a href="' . esc_url($brand_link) . '">' . esc_html($brand_title) . '</a>';
+        } else {
+            echo __('No Brand Assigned', 'text_domain');
+        }
+    }
+}
+add_action('manage_landing-page_posts_custom_column', 'tp_custom_landing_page_column', 10, 2);
+
+// Make the custom columns sortable
+function tp_sortable_listing_page_columns($columns) {
+    $columns['assigned_brand'] = 'assigned_brand';
+    return $columns;
+}
+add_filter('manage_edit-listing-page_sortable_columns', 'tp_sortable_listing_page_columns');
+
+function tp_sortable_landing_page_columns($columns) {
+    $columns['assigned_brand'] = 'assigned_brand';
+    return $columns;
+}
+add_filter('manage_edit-landing-page_sortable_columns', 'tp_sortable_landing_page_columns');
 
 ?>
