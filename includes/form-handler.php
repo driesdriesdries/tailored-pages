0bd072c12dd5b123dc726d18583640740c969f27
Andries Bester <andriesbester@Andriess-MacBook-Air.local>
2024-06-02 17:42:26 +0200

    leads fire into table. need to transform into hidden fields

---
+++ includes/form-handler.php
@@ -0,0 +1,138 @@
+<?php
+function tp_handle_form_submission() {
+    if (isset($_POST['action']) && $_POST['action'] === 'submit_landing_page_form') {
+        global $wpdb;
+
+        // Sanitize and process form input
+        $first_name = sanitize_text_field($_POST['first_name']);
+        $last_name = sanitize_text_field($_POST['last_name']);
+        $email_address = sanitize_email($_POST['email_address']);
+        $marketing_consent = isset($_POST['marketing_consent']) ? 1 : 0;
+        $time = current_time('mysql');
+        $landing_page_id = intval($_POST['landing_page_id']);
+
+        // Insert data into custom table
+        $table_name = $wpdb->prefix . 'tp_leads';
+        $wpdb->insert(
+            $table_name,
+            array(
+                'time' => $time,
+                'first_name' => $first_name,
+                'last_name' => $last_name,
+                'email_address' => $email_address,
+                'marketing_consent' => $marketing_consent,
+            )
+        );
+
+        // Check if there's a cached success page URL
+        $cache_key = 'success_page_' . $landing_page_id;
+        $success_page_url = get_transient($cache_key);
+
+        if ($success_page_url === false) {
+            // Query for the success page that has this landing page selected in its ACF field
+            $success_page_query = new WP_Query(array(
+                'post_type' => 'success-page',
+                'meta_query' => array(
+                    array(
+                        'key' => 'landing_page_source', // ACF field name
+                        'value' => '"' . $landing_page_id . '"',
+                        'compare' => 'LIKE'
+                    )
+                )
+            ));
+
+            if ($success_page_query->have_posts()) {
+                $success_page_query->the_post();
+                $success_page_url = get_permalink();
+                wp_reset_postdata();
+                // Cache the success page URL for 12 hours
+                set_transient($cache_key, $success_page_url, 12 * HOUR_IN_SECONDS);
+            } else {
+                $success_page_url = home_url('/');
+            }
+        }
+
+        // Redirect after processing
+        wp_redirect($success_page_url);
+        exit;
+    }
+}
+add_action('admin_post_nopriv_submit_landing_page_form', 'tp_handle_form_submission');
+add_action('admin_post_submit_landing_page_form', 'tp_handle_form_submission');
+
+// Custom table creation function
+function tp_create_custom_table() {
+    global $wpdb;
+    $table_name = $wpdb->prefix . 'tp_leads';
+    $charset_collate = $wpdb->get_charset_collate();
+
+    $sql = "CREATE TABLE $table_name (
+        id mediumint(9) NOT NULL AUTO_INCREMENT,
+        time datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
+        first_name tinytext NOT NULL,
+        last_name tinytext NOT NULL,
+        email_address varchar(100) NOT NULL,
+        marketing_consent boolean DEFAULT false NOT NULL,
+        associated_brand tinytext NOT NULL,
+        associated_product tinytext NOT NULL,
+        PRIMARY KEY  (id)
+    ) $charset_collate;";
+
+    require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+    dbDelta($sql);
+
+    if ($wpdb->last_error) {
+        error_log('Custom Table Creation Error: ' . $wpdb->last_error);
+    } else {
+        error_log('Custom Table Created Successfully: ' . $table_name);
+    }
+}
+
+
+function tp_register_export_page() {
+    add_submenu_page(
+        'edit.php?post_type=landing-page',
+        __('Export Leads', 'text_domain'),
+        __('Export Leads', 'text_domain'),
+        'manage_options',
+        'export-leads',
+        'tp_export_leads_page'
+    );
+}
+add_action('admin_menu', 'tp_register_export_page');
+
+function tp_export_leads_page() {
+    ?>
+    <div class="wrap">
+        <h1><?php _e('Export Leads', 'text_domain'); ?></h1>
+        <form method="post" action="">
+            <input type="hidden" name="tp_export_csv" value="1">
+            <?php submit_button(__('Export to CSV', 'text_domain')); ?>
+        </form>
+    </div>
+    <?php
+}
+
+function tp_export_leads() {
+    if (isset($_POST['tp_export_csv']) && current_user_can('manage_options')) {
+        global $wpdb;
+        $table_name = $wpdb->prefix . 'tp_leads';
+
+        $results = $wpdb->get_results("SELECT * FROM $table_name", ARRAY_A);
+
+        if (!empty($results)) {
+            header('Content-Type: text/csv; charset=utf-8');
+            header('Content-Disposition: attachment; filename=leads.csv');
+            $output = fopen('php://output', 'w');
+            fputcsv($output, array('ID', 'Time', 'First Name', 'Last Name', 'Email Address', 'Marketing Consent'));
+
+            foreach ($results as $row) {
+                fputcsv($output, $row);
+            }
+
+            fclose($output);
+            exit;
+        }
+    }
+}
+add_action('admin_init', 'tp_export_leads');
--- tailored-pages.php
+++ tailored-pages.php
@@ -6,33 +6,37 @@
  * Author: Andries Bester
  */
 
-// Include admin menus
+// Include necessary files
 require_once plugin_dir_path(__FILE__) . 'includes/admin/admin-menus.php';
 require_once plugin_dir_path(__FILE__) . 'includes/custom-post-types.php';
 require_once plugin_dir_path(__FILE__) . 'includes/public/public-scripts.php';
 require_once plugin_dir_path(__FILE__) . 'includes/meta-boxes.php';
 require_once plugin_dir_path(__FILE__) . 'includes/custom-columns.php';
+require_once plugin_dir_path(__FILE__) . 'includes/form-handler.php';
+
+// Custom table creation on plugin activation
+register_activation_hook(__FILE__, 'tp_create_custom_table');
 
 // Include the selected template
-function tp_include_template( $template ) {
-    if ( is_singular(['brochure', 'landing-page', 'listing-page', 'success-page']) ) {
+function tp_include_template($template) {
+    if (is_singular(['brochure', 'landing-page', 'listing-page', 'success-page'])) {
         global $post;
-        $selected_template = get_post_meta( $post->ID, '_tp_selected_template', true );
-        if ( $selected_template ) {
+        $selected_template = get_post_meta($post->ID, '_tp_selected_template', true);
+        if ($selected_template) {
             $template_directory = '';
             $template_file = $selected_template . '.php';
-            if ( strpos( $selected_template, 'landing-page' ) !== false ) {
+            if (strpos($selected_template, 'landing-page') !== false) {
                 $template_directory = 'landing-pages';
-            } elseif ( strpos( $selected_template, 'brochure' ) !== false ) {
+            } elseif (strpos($selected_template, 'brochure') !== false) {
                 $template_directory = 'brochures';
-            } elseif ( strpos( $selected_template, 'listing-page' ) !== false ) {
+            } elseif (strpos($selected_template, 'listing-page') !== false) {
                 $template_directory = 'listing-pages';
-            } elseif ( strpos( $selected_template, 'success-page' ) !== false ) {
+            } elseif (strpos($selected_template, 'success-page') !== false) {
                 $template_directory = 'success-pages';
             }
 
-            $template_path = plugin_dir_path( __FILE__ ) . 'templates/' . $template_directory . '/' . $template_file;
-            if ( file_exists( $template_path ) ) {
+            $template_path = plugin_dir_path(__FILE__) . 'templates/' . $template_directory . '/' . $template_file;
+            if (file_exists($template_path)) {
                 error_log('Using custom template: ' . $template_path);
                 return $template_path;
             } else {
@@ -46,14 +50,13 @@ function tp_include_template( $template ) {
     }
     return $template;
 }
-add_filter( 'template_include', 'tp_include_template' );
-
+add_filter('template_include', 'tp_include_template');
 
 function tp_enqueue_admin_scripts($hook) {
     if ('post.php' != $hook && 'post-new.php' != $hook) {
         return;
     }
-    
+
     global $post;
     if ('landing-page' !== $post->post_type) {
         return;
@@ -95,95 +98,7 @@ function tp_enqueue_admin_scripts($hook) {
 }
 add_action('admin_enqueue_scripts', 'tp_enqueue_admin_scripts');
 
-// add_filter('acf/fields/relationship/query/key=field_6659a1c39636d', 'tp_filter_products_by_brand', 10, 3);
-// function tp_filter_products_by_brand($args, $field, $post_id) {
-//     // Get the associated brand ID
-//     $associated_brand = get_field('associated_brand', $post_id);
-
-//     if ($associated_brand) {
-//         // Ensure it's an array of post objects and get the ID
-//         if (is_array($associated_brand)) {
-//             $associated_brand = $associated_brand[0]->ID;
-//         } else {
-//             $associated_brand = $associated_brand->ID;
-//         }
-
-//         // Modify the query arguments to filter by associated brand
-//         $args['meta_query'] = array(
-//             array(
-//                 'key' => 'associated_brand', // The custom field key in the product post type
-//                 'value' => '"' . $associated_brand . '"',
-//                 'compare' => 'LIKE'
-//             )
-//         );
-//     }
-
-//     return $args;
-// }
-
-function tp_handle_form_submission() {
-    if (isset($_POST['action']) && $_POST['action'] === 'submit_landing_page_form') {
-        // Sanitize and process form input
-        $first_name = sanitize_text_field($_POST['first_name']);
-        $last_name = sanitize_text_field($_POST['last_name']);
-        $email_address = sanitize_email($_POST['email_address']);
-        $marketing_consent = isset($_POST['marketing_consent']) ? 1 : 0;
-        $landing_page_id = intval($_POST['landing_page_id']);
-
-        // Save the data to a custom post type
-        $new_post = array(
-            'post_title'    => $first_name . ' ' . $last_name,
-            'post_type'     => 'lead',
-            'post_status'   => 'publish',
-        );
-
-        $post_id = wp_insert_post($new_post);
-
-        if ($post_id) {
-            update_post_meta($post_id, 'email_address', $email_address);
-            update_post_meta($post_id, 'marketing_consent', $marketing_consent);
-        }
-
-        // Check if there's a cached success page URL
-        $cache_key = 'success_page_' . $landing_page_id;
-        $success_page_url = get_transient($cache_key);
-
-        if ($success_page_url === false) {
-            // Query for the success page that has this landing page selected in its ACF field
-            $success_page_query = new WP_Query(array(
-                'post_type' => 'success-page',
-                'meta_query' => array(
-                    array(
-                        'key' => 'landing_page_source', // ACF field name
-                        'value' => '"' . $landing_page_id . '"',
-                        'compare' => 'LIKE'
-                    )
-                )
-            ));
-
-            if ($success_page_query->have_posts()) {
-                $success_page_query->the_post();
-                $success_page_url = get_permalink();
-                wp_reset_postdata();
-                // Cache the success page URL for 12 hours
-                set_transient($cache_key, $success_page_url, 12 * HOUR_IN_SECONDS);
-            } else {
-                $success_page_url = home_url('/');
-            }
-        }
-
-        // Redirect after processing
-        wp_redirect($success_page_url);
-        exit;
-    }
-}
-add_action('admin_post_nopriv_submit_landing_page_form', 'tp_handle_form_submission');
-add_action('admin_post_submit_landing_page_form', 'tp_handle_form_submission');
-
-
 add_action('after_setup_theme', 'custom_image_sizes');
 function custom_image_sizes() {
     add_image_size('hero-background', 1920, 1080, true); // Example size, adjust as needed
 }
-
-?>
