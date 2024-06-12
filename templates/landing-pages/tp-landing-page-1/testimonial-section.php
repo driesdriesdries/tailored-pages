9b6112374d91ae5bfac8829180598276e7fdab30
Andries Bester <andriesbester@Andriess-MacBook-Air.local>
2024-06-12 12:28:53 +0200

    added testimonials on the front end

--- templates/landing-pages/tp-landing-page-1/testimonial-section.php
+++ templates/landing-pages/tp-landing-page-1/testimonial-section.php
@@ -1,4 +1,29 @@
 <!-- Testimonial Area -->
 <section class="testimonial-section">
-    <h1>Kokkie</h1>
+    <h2>Read all these fokken Testimonials</h2>
+    <p>ou're welcome! If you have any more sections or queries that need optimization, feel free to share them</p>
+    <div class="testimonial-group">
+        <div class="testimonial-group-instance">
+            <p class="testimonial-group-instance-copy">“Andries used his wealth of experience and understanding of how humans learn best to foster a culture of growth in the Web Operations team that he led during my time at 2U. Always being keen to innovate and push the boundaries of what is possible made Andries a key component of a very successful team - he would be an absolute asset to any brand looking to drive hyper growth in the digital space.”</p>
+            <img src="https://i.kym-cdn.com/entries/icons/facebook/000/045/146/son-goku-thumb-up.jpg" alt="#" class="testimonial-group-instance-portrait">
+            <p class="testimonial-group-instance-name">Andries Bester</p>
+            <p class="testimonial-group-instance-title">Loser of Note</p>
+            <img src="https://i.kym-cdn.com/entries/icons/facebook/000/045/146/son-goku-thumb-up.jpg" alt="" class="testimonial-group-instance-logo">
+        </div>
+
+        <div class="testimonial-group-instance">
+            <p class="testimonial-group-instance-copy">“Andries used his wealth of experience and understanding of how humans learn best to foster a culture of growth in the Web Operations team that he led during my time at 2U. Always being keen to innovate and push the boundaries of what is possible made Andries a key component of a very successful team - he would be an absolute asset to any brand looking to drive hyper growth in the digital space.”</p>
+            <img src="https://i.kym-cdn.com/entries/icons/facebook/000/045/146/son-goku-thumb-up.jpg" alt="#" class="testimonial-group-instance-portrait">
+            <p class="testimonial-group-instance-name">Andries Bester</p>
+            <p class="testimonial-group-instance-title">Loser of Note</p>
+            <img src="https://i.kym-cdn.com/entries/icons/facebook/000/045/146/son-goku-thumb-up.jpg" alt="" class="testimonial-group-instance-logo">
+        </div>
+        <div class="testimonial-group-instance">
+            <p class="testimonial-group-instance-copy">“Andries used his wealth of experience and understanding of how humans learn best to foster a culture of growth in the Web Operations team that he led during my time at 2U. Always being keen to innovate and push the boundaries of what is possible made Andries a key component of a very successful team - he would be an absolute asset to any brand looking to drive hyper growth in the digital space.”</p>
+            <img src="https://i.kym-cdn.com/entries/icons/facebook/000/045/146/son-goku-thumb-up.jpg" alt="#" class="testimonial-group-instance-portrait">
+            <p class="testimonial-group-instance-name">Andries Bester</p>
+            <p class="testimonial-group-instance-title">Loser of Note</p>
+            <img src="https://i.kym-cdn.com/entries/icons/facebook/000/045/146/son-goku-thumb-up.jpg" alt="" class="testimonial-group-instance-logo">
+        </div>
+    </div>
 </section>
