93968af4b94348adcf1b968600ba84d00eee00b2
Andries Bester <andriesbester@Andriess-MacBook-Air.local>
2024-06-12 14:39:22 +0200

    more

--- templates/landing-pages/tp-landing-page-1/testimonial-section.php
+++ templates/landing-pages/tp-landing-page-1/testimonial-section.php
@@ -4,18 +4,20 @@ $queries_before = get_num_queries();
 global $wpdb;
 $queries_before_list = $wpdb->queries;
 
-// Get the testimonial section background color, text color, instance background color, and instance text color using get_post_meta
+// Get the testimonial section background color, text color, instance background color, instance text color, and instance accent color using get_post_meta
 $post_id = get_the_ID();
 $testimonial_background_color = get_post_meta($post_id, 'testimonial_section_background_color', true);
 $testimonial_text_color = get_post_meta($post_id, 'testimonial_section_text_color', true);
 $testimonial_instance_background_color = get_post_meta($post_id, 'testimonial_instance_background_color', true);
 $testimonial_instance_text_color = get_post_meta($post_id, 'testimonial_instance_text_color', true);
+$testimonial_instance_accent_color = get_post_meta($post_id, 'testimonial_instance_accent_color', true);
 
 // Prepare the color values
 $background_color_value = isset($brand_colors[$testimonial_background_color]) ? $brand_colors[$testimonial_background_color] : '#FFFFFF';
 $text_color_value = isset($brand_colors[$testimonial_text_color]) ? $brand_colors[$testimonial_text_color] : '#000000';
 $instance_background_color_value = isset($brand_colors[$testimonial_instance_background_color]) ? $brand_colors[$testimonial_instance_background_color] : '#F0F0F0';
 $instance_text_color_value = isset($brand_colors[$testimonial_instance_text_color]) ? $brand_colors[$testimonial_instance_text_color] : '#000000';
+$instance_accent_color_value = isset($brand_colors[$testimonial_instance_accent_color]) ? $brand_colors[$testimonial_instance_accent_color] : '#FF0000';
 ?>
 
 <!-- Testimonial Area -->
@@ -23,7 +25,7 @@ $instance_text_color_value = isset($brand_colors[$testimonial_instance_text_colo
     <h2 style="color: <?php echo esc_attr($text_color_value); ?>;">Read all these fokken Testimonials</h2>
     <p style="color: <?php echo esc_attr($text_color_value); ?>;">You're welcome! If you have any more sections or queries that need optimization, feel free to share them</p>
     <div class="testimonial-group">
-        <div class="testimonial-group-instance" style="background-color: <?php echo esc_attr($instance_background_color_value); ?>;">
+        <div class="testimonial-group-instance" style="background-color: <?php echo esc_attr($instance_background_color_value); ?>; border-top: 5px solid <?php echo esc_attr($instance_accent_color_value); ?>;">
             <p class="testimonial-group-instance-copy" style="color: <?php echo esc_attr($instance_text_color_value); ?>;">“Andries used his wealth of experience and understanding of how humans learn best to foster a culture of growth in the Web Operations team that he led during my time at 2U. Always being keen to innovate and push the boundaries of what is possible made Andries a key component of a very successful team - he would be an absolute asset to any brand looking to drive hyper growth in the digital space.”</p>
             <img src="https://i.kym-cdn.com/entries/icons/facebook/000/045/146/son-goku-thumb-up.jpg" alt="#" class="testimonial-group-instance-portrait">
             <p class="testimonial-group-instance-name">Andries Bester</p>
@@ -31,7 +33,7 @@ $instance_text_color_value = isset($brand_colors[$testimonial_instance_text_colo
             <img src="https://i.kym-cdn.com/entries/icons/facebook/000/045/146/son-goku-thumb-up.jpg" alt="" class="testimonial-group-instance-logo">
         </div>
 
-        <div class="testimonial-group-instance" style="background-color: <?php echo esc_attr($instance_background_color_value); ?>;">
+        <div class="testimonial-group-instance" style="background-color: <?php echo esc_attr($instance_background_color_value); ?>; border-top: 5px solid <?php echo esc_attr($instance_accent_color_value); ?>;">
             <p class="testimonial-group-instance-copy" style="color: <?php echo esc_attr($instance_text_color_value); ?>;">“Andries used his wealth of experience and understanding of how humans learn best to foster a culture of growth in the Web Operations team that he led during my time at 2U. Always being keen to innovate and push the boundaries of what is possible made Andries a key component of a very successful team - he would be an absolute asset to any brand looking to drive hyper growth in the digital space.”</p>
             <img src="https://i.kym-cdn.com/entries/icons/facebook/000/045/146/son-goku-thumb-up.jpg" alt="#" class="testimonial-group-instance-portrait">
             <p class="testimonial-group-instance-name">Andries Bester</p>
@@ -39,7 +41,7 @@ $instance_text_color_value = isset($brand_colors[$testimonial_instance_text_colo
             <img src="https://i.kym-cdn.com/entries/icons/facebook/000/045/146/son-goku-thumb-up.jpg" alt="" class="testimonial-group-instance-logo">
         </div>
         
-        <div class="testimonial-group-instance" style="background-color: <?php echo esc_attr($instance_background_color_value); ?>;">
+        <div class="testimonial-group-instance" style="background-color: <?php echo esc_attr($instance_background_color_value); ?>; border-top: 5px solid <?php echo esc_attr($instance_accent_color_value); ?>;">
             <p class="testimonial-group-instance-copy" style="color: <?php echo esc_attr($instance_text_color_value); ?>;">“Andries used his wealth of experience and understanding of how humans learn best to foster a culture of growth in the Web Operations team that he led during my time at 2U. Always being keen to innovate and push the boundaries of what is possible made Andries a key component of a very successful team - he would be an absolute asset to any brand looking to drive hyper growth in the digital space.”</p>
             <img src="https://i.kym-cdn.com/entries/icons/facebook/000/045/146/son-goku-thumb-up.jpg" alt="#" class="testimonial-group-instance-portrait">
             <p class="testimonial-group-instance-name">Andries Bester</p>
