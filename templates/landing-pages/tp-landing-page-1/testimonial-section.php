c151ff63a2cc2b9365152b7d3755e3db30f3f33a
Andries Bester <andriesbester@Andriess-MacBook-Air.local>
2024-06-12 14:29:25 +0200

    more

--- templates/landing-pages/tp-landing-page-1/testimonial-section.php
+++ templates/landing-pages/tp-landing-page-1/testimonial-section.php
@@ -8,10 +8,12 @@ $queries_before_list = $wpdb->queries;
 $post_id = get_the_ID();
 $testimonial_background_color = get_post_meta($post_id, 'testimonial_section_background_color', true);
 $testimonial_text_color = get_post_meta($post_id, 'testimonial_section_text_color', true);
+$testimonial_instance_background_color = get_post_meta($post_id, 'testimonial_instance_background_color', true);
 
-// Prepare the background color value
+// Prepare the color values
 $background_color_value = isset($brand_colors[$testimonial_background_color]) ? $brand_colors[$testimonial_background_color] : '#FFFFFF';
 $text_color_value = isset($brand_colors[$testimonial_text_color]) ? $brand_colors[$testimonial_text_color] : '#000000';
+$instance_background_color_value = isset($brand_colors[$testimonial_instance_background_color]) ? $brand_colors[$testimonial_instance_background_color] : '#F0F0F0';
 ?>
 
 <!-- Testimonial Area -->
@@ -19,7 +21,7 @@ $text_color_value = isset($brand_colors[$testimonial_text_color]) ? $brand_color
     <h2 style="color: <?php echo esc_attr($text_color_value); ?>;">Read all these fokken Testimonials</h2>
     <p style="color: <?php echo esc_attr($text_color_value); ?>;">You're welcome! If you have any more sections or queries that need optimization, feel free to share them</p>
     <div class="testimonial-group">
-        <div class="testimonial-group-instance">
+        <div class="testimonial-group-instance" style="background-color: <?php echo esc_attr($instance_background_color_value); ?>;">
             <p class="testimonial-group-instance-copy">“Andries used his wealth of experience and understanding of how humans learn best to foster a culture of growth in the Web Operations team that he led during my time at 2U. Always being keen to innovate and push the boundaries of what is possible made Andries a key component of a very successful team - he would be an absolute asset to any brand looking to drive hyper growth in the digital space.”</p>
             <img src="https://i.kym-cdn.com/entries/icons/facebook/000/045/146/son-goku-thumb-up.jpg" alt="#" class="testimonial-group-instance-portrait">
             <p class="testimonial-group-instance-name">Andries Bester</p>
@@ -27,14 +29,15 @@ $text_color_value = isset($brand_colors[$testimonial_text_color]) ? $brand_color
             <img src="https://i.kym-cdn.com/entries/icons/facebook/000/045/146/son-goku-thumb-up.jpg" alt="" class="testimonial-group-instance-logo">
         </div>
 
-        <div class="testimonial-group-instance">
+        <div class="testimonial-group-instance" style="background-color: <?php echo esc_attr($instance_background_color_value); ?>;">
             <p class="testimonial-group-instance-copy">“Andries used his wealth of experience and understanding of how humans learn best to foster a culture of growth in the Web Operations team that he led during my time at 2U. Always being keen to innovate and push the boundaries of what is possible made Andries a key component of a very successful team - he would be an absolute asset to any brand looking to drive hyper growth in the digital space.”</p>
             <img src="https://i.kym-cdn.com/entries/icons/facebook/000/045/146/son-goku-thumb-up.jpg" alt="#" class="testimonial-group-instance-portrait">
             <p class="testimonial-group-instance-name">Andries Bester</p>
             <p class="testimonial-group-instance-title">Loser of Note</p>
             <img src="https://i.kym-cdn.com/entries/icons/facebook/000/045/146/son-goku-thumb-up.jpg" alt="" class="testimonial-group-instance-logo">
         </div>
-        <div class="testimonial-group-instance">
+        
+        <div class="testimonial-group-instance" style="background-color: <?php echo esc_attr($instance_background_color_value); ?>;">
             <p class="testimonial-group-instance-copy">“Andries used his wealth of experience and understanding of how humans learn best to foster a culture of growth in the Web Operations team that he led during my time at 2U. Always being keen to innovate and push the boundaries of what is possible made Andries a key component of a very successful team - he would be an absolute asset to any brand looking to drive hyper growth in the digital space.”</p>
             <img src="https://i.kym-cdn.com/entries/icons/facebook/000/045/146/son-goku-thumb-up.jpg" alt="#" class="testimonial-group-instance-portrait">
             <p class="testimonial-group-instance-name">Andries Bester</p>
